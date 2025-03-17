692595f05b7fffe88c0adf1873b8a86cb3af2ead
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2025-03-17 09:33:32 +0100

    Removed read_constant from Value

--- src/value.rs
+++ src/value.rs
@@ -4,13 +4,6 @@ use crate::Sequence;
 #[derive(Debug)]
 pub struct Value(f32);
 
-impl Value {
-    pub fn read_constant(sequence: &[u8])-> Self {
-        let value = f32::from(sequence[0]);
-        Value(value)
-    }
-}
-
 impl From<f32> for Value {
     fn from(value: f32) -> Self {
         Self(value)

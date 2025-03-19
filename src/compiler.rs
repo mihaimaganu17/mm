d2e023fbe46955a3506420128de7af04c04397b7
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2025-03-19 10:46:18 +0100

    Scan byte by byte

--- src/compiler.rs
+++ src/compiler.rs
@@ -4,7 +4,11 @@ pub struct Compiler;
 
 impl Compiler {
     pub fn compile(&self, bytes: &[u8]) -> Result<(), CompileError> {
-        let _scanner = Scanner::new(bytes);
+        let mut scanner = Scanner::new(bytes);
+        // Keeps scanning and compiling tokens until the end of `bytes`
+        while let Some(maybe_token) = scanner.next_token() {
+            println!("{:?}", maybe_token);
+        }
         Ok(())
     }
 }
--- src/scan.rs
+++ src/scan.rs
@@ -1,6 +1,8 @@
 #[derive(Debug)]
 pub struct Scanner<'a> {
     data: &'a [u8],
+    // Offset that marks the beginning of the current lexeme being scanned
+    start: usize,
     // Current offset in the `data` field
     offset: usize,
     // The line the cursor is on
@@ -12,8 +14,19 @@ impl<'a> Scanner<'a> {
     pub fn new(data: &'a [u8]) -> Self {
         Self {
             data,
+            start: 0,
             offset: 0,
             line: 1,
+        }
+    }
+
+    pub fn next_token(&mut self) -> Option<Result<u8, ScanError>> {
+        if self.offset>= self.data.len() {
+            None
+        } else {
+            let curr = self.data[self.offset];
+            self.offset = self.offset.checked_add(1)?;
+            Some(Ok(curr))
         }
     }
 }
